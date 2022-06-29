5721ff07196d255612c5e0a25002838650eb97c3
Samuel Oduba <samueloduba123@gmail.com>
2022-06-29 00:36:45 -0700

    update

--- app.js
+++ app.js
@@ -2,23 +2,9 @@ const dataSet = JSON.parse(data);
 const tableSearch = document.getElementById('search-input')
 const btn = document.getElementById('search-btn')
 
-// btn.addEventListener('click', function(){
-//     let value = document.getElementById('search-input').value
-
-//     const filterDataSet = dataSet.filter(function(data) {
-//         value = value.toLowerCase()
-   
-//         return data.first_name.toLowerCase().includes(value) ||
-//             data.last_name.toLowerCase().includes(value) ||
-//             data.bvn.includes(value) ||
-//             data.avaliable_balance.toString().includes(value) ||
-//             data.account_opened_at.includes(value) 
-//     })
-//     loadTable(filterDataSet)
-// })
-
 tableSearch.addEventListener('keydown', function(){
     let value = document.getElementById('search-input').value
+
 
     const filterDataSet = dataSet.filter(function(data) {
         value = value.toLowerCase()
@@ -32,23 +18,10 @@ tableSearch.addEventListener('keydown', function(){
     loadTable(filterDataSet)
 })
 
-
-
-
-
-
-
-
 // TODO: Implement Search
 // TODO: Implement: Sort
 
-
 loadTable(dataSet)
-
-
-
-
-
 
 function loadTable(dataSet){
     const tableData = document.getElementById('tableData')
@@ -69,3 +42,27 @@ function loadTable(dataSet){
     tableData.innerHTML = dataHtml;
 
 }
+
+const arrow = document.getElementById('arrow')
+
+
+
+arrow.addEventListener('click', filtetByFirstName)
+
+
+function filtetByFirstName(){
+      const sortData = dataSet.sort(function(name1, name2) {
+    const nameA = name1.first_name
+    const nameB = name2.first_name
+        if (nameA < nameB) {
+        return -1;
+  }
+        if (nameA > nameB) {
+        return 1;
+  }
+        return 0
+    })
+    loadTable(sortData)
+
+}
+
